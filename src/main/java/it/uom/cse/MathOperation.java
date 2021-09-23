70626e4b15a8c48f0e9617e65fb609c2920f0f57
My name <raveenda00@gmail.com>
2021-09-23 11:26:41 +0530

    CSE-LIBRARY: MathOperation sumOfPrimes

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -14,5 +14,28 @@ public class MathOperation {
     {
         return operand1 - operand2;
     }
+    public static int sumOfPrimes(n){
+    	int i, number, count, sum = 0 
+			
+		for(number = 1; number <= 100; number++)
+		{
+			count = 0;
+		    for (i = 2; i <= number/2; i++)
+		    {
+		    	if(number % i == 0)
+		    	{
+		    		count++;
+		    		break;
+		    	}
+		    }
+		    if(count == 0 && number != 1 )
+		    {
+		    	sum = sum + number;
+		    }  
+		}
+		
+	System.out.println("sum of primes; " +sum);
+} 
+}   
 
-}
+
